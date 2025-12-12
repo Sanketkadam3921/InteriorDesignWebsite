86b731d5615fa1b0b6f5601f5c6e7aab68c3060d
Sanket Kadam <sanketkadam3921@gmail.com>
2025-12-12 15:28:17 +0530

    new commit

--- src/components/pages/second-nav-pages/offerings/ModularInteriors/index.jsx
+++ src/components/pages/second-nav-pages/offerings/ModularInteriors/index.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import { Box, Container } from '@mui/material';
-import HeroSection from './HeroSection';
-import Introduction from './Introduction';
-import WhatWeOffer from './WhatWeOffer';
-import WhyChoose from './WhyChoose';
-import ClosingCTA from './ClosingCTA';
-import Testimonials from './Testimonials';
-import FAQ from './FAQ';
+import React from "react";
+import { Box, Container } from "@mui/material";
+import HeroSection from "./HeroSection";
+import Introduction from "./Introduction";
+import WhatWeOffer from "./WhatWeOffer";
+import WhyChoose from "./WhyChoose";
+import ClosingCTA from "./ClosingCTA";
+import Testimonials from "./Testimonials";
+import FAQ from "./FAQ";
 
 export default function ModularInteriors() {
-    return (
-        <Box sx={{
-            overflowX: 'hidden',
-            width: '100%',
-            minHeight: '100vh',
-            position: 'relative'
-        }}>
-            <HeroSection />
+  return (
+    <Box
+      sx={{
+        overflowX: "hidden",
+        width: "100%",
+        minHeight: "100vh",
+        position: "relative",
+      }}
+    >
+      <HeroSection />
 
-            <Container maxWidth="lg" sx={{ py: 6, px: { xs: 2, sm: 3, md: 4 } }}>
-                <Introduction />
-                <WhatWeOffer />
-                <WhyChoose />
-                <ClosingCTA />
-            </Container>
+      <Container maxWidth="lg" sx={{ py: 6, px: { xs: 2, sm: 3, md: 4 } }}>
+        <Introduction />
+        <WhatWeOffer />
+        <WhyChoose />
+        {/* <ClosingCTA /> */}
+      </Container>
 
-            {/* <Testimonials /> */}
-            <FAQ />
-        </Box>
-    );
+      {/* <Testimonials /> */}
+      <FAQ />
+    </Box>
+  );
 }
-
